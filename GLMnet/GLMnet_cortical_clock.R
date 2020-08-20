290d13356c39aad2f45b2272de68a5d492e46902
Gemma S <gs470@exeter.ac.uk>
2020-08-20 09:40:15 +0100

    Update GLMnet_cortical_clock.R

--- GLMnet/GLMnet_cortical_clock.R
+++ GLMnet/GLMnet_cortical_clock.R
@@ -32,19 +32,11 @@ test<-read.csv("testing_sample.csv")
 
 trafo= function(x,adult.age=20) { x=(x+1)/(1+adult.age); y=ifelse(x<=1, log( x),x-1);y }
 test$AgeT=trafo(test$Age)
-
-
 train$AgeT=trafo(train$Age)
 
 ## transform back at the end 
 
 anti.trafo<-function(x,adult.age=20) { ifelse(x<0, (1+adult.age)*exp(x)-1, (1+adult.age)*x+adult.age) }
-test$AgeAT<-anti.trafo(test$AgeT)
-
-
-train$AgeAT<-anti.trafo(train$AgeT)
-
-
 
 #################################################################################################################
 ################################################ MATCH TO BETAS  ################################################
